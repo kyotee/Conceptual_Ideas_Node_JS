a30de46fb63a2acaede79ab10ecc7cebb5264852
tamkylet <kyle_tam1@hotmail.com>
2019-04-05 02:02:43 -0700

    react components + redux + routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,28 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Notfound from './notfound';
 import App from './javascripts/ui/app.js';
+import App1 from './javascripts/ui/app1.js';
+import App2 from './javascripts/ui/app2.js';
+import CounterRedux from './javascripts/containers/counterRedux.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/app1" component={App1} />
+        <Route path="/app2" component={App2} />
+        <Route
+          path='/app3'
+          render={(props) => <CounterRedux {...props} counterNum={10} />}
+        />
+        <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
