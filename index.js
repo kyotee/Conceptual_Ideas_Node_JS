65c346be3a8a46845f6cdeb52e5fc4a453da8f67
tamkylet <kyle_tam1@hotmail.com>
2019-04-05 03:07:02 -0700

    database connection

--- index.js
+++ index.js
@@ -2,36 +2,62 @@ const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
 
-var { test } = require("./routes/test");
+var { test, testdb } = require("./routes/test");
 
 const app = express();
+
+// MySQL database connection
+const mysql = require('mysql');
+
+if (process.env.NODE_ENV === "production") {
+  const con = mysql.createConnection({
+    // server database for production
+    host     : 'us-cdbr-iron-east-03.cleardb.net',
+    user     : 'b1716937e432f2',
+    password : '5bb18616',
+    database : 'heroku_9705d094c064be3'
+  });
+
+  con.connect((err) => {
+    if(err){
+      console.log('Error connecting to Db');
+      return;
+    }
+    console.log('Connection established');
+  });
+
+  global.con = con;
+} else {
+  const con = mysql.createConnection({
+    // local database for development
+    host     : 'localhost',
+    user     : 'admin',
+    password : 'admin1',
+    database : 'conceptual_ideas_node_db'
+  });
+
+  con.connect((err) => {
+    if(err){
+      console.log('Error connecting to Db');
+      return;
+    }
+    console.log('Connection established');
+  });
+
+  global.con = con;
+}
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get("/api/test", test);
+app.get("/api/testdb", testdb);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//     next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//     // set locals, only providing error in development
-//     res.locals.message = err.message;
-//     res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//     // render the error page
-//     res.status(err.status || 500);
-//     res.render("error");
-// });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
--- routes/test.js
+++ routes/test.js
@@ -5,5 +5,19 @@ module.exports = {
 		let r = Math.random().toString(36).substring(7);
 		res.send(r);
 		console.log("random", r);
+	},
+
+	// show
+    testdb: (req, res) => {
+    	let query = "SELECT * FROM `employees`"; 
+
+		con.query(query, (err,rows) => {
+			if (err) {
+				// res.redirect('/');
+			}
+
+			res.send(rows);
+			console.log(rows);
+		});
 	}
 };
