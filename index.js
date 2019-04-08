fe4210246063f2ad49777ac6081ee0f9d57e1a13
tamkylet <kyle_tam1@hotmail.com>
2019-04-07 17:59:41 -0700

    index controller (route) refactor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
 
-var { test, testdb } = require("./controllers/test");
+var { test, testdb } = require("./controllers");
 
 const app = express();
 
