9e4bd22d5b47914a5de8c7ebe1907debab4d6f1d
tamkylet <kyle_tam1@hotmail.com>
2019-04-08 20:11:36 -0700

    navigation react component

--- views/src/index.js
+++ views/src/index.js
@@ -3,10 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Notfound from './notfound';
+import NavigationBar from './javascripts/ui/navigation_bar.js';
 import UserApplications from './javascripts/ui/user_applications.js'; 
 import App from './javascripts/ui/app.js';
 import App1 from './javascripts/ui/app1.js';
 import CounterRedux from './javascripts/containers/counterRedux.js';
+
+ReactDOM.render(<NavigationBar/>, document.getElementById('nav'));
 
 const routing = (
   <Router>
---
+++ views/src/javascripts/ui/navigation_bar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import "../../stylesheets/navigation_bar.scss";
+
+class NavigationBar extends Component {
+	componentDidMount() {
+		document.getElementById('name-container').addEventListener("click", function() {
+			window.location = "/";
+		});
+
+		document.getElementById('signing-up').addEventListener("click", function() {
+			window.location = "/";
+		});
+	}
+	loggedin() {
+		return (
+			<div id="login-signup">
+				<p id="signing-up">Sign up</p>
+				<p><a href="#" id="signin_link">Sign in</a></p>
+			</div>
+		)
+	}
+	render() {
+		return (
+			<div id="nav">
+				<div id="name-container">
+					<div id="idea-icon"></div>
+					<p id="title1">Conceptual</p>
+					<p id="title2">Ideas</p>
+				</div>
+
+				{this.loggedin()}
+			</div>
+		)
+	}
+}
+
+export default NavigationBar;
