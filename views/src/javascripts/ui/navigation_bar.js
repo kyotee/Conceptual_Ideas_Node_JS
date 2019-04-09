103759d9a463b8492d65cb975ca96678ca41730f
tamkylet <kyle_tam1@hotmail.com>
2019-04-08 20:28:06 -0700

    title tweak

--- views/src/javascripts/ui/navigation_bar.js
+++ views/src/javascripts/ui/navigation_bar.js
@@ -26,6 +26,7 @@ class NavigationBar extends Component {
 					<div id="idea-icon"></div>
 					<p id="title1">Conceptual</p>
 					<p id="title2">Ideas</p>
+					<p id="title3">Node.js</p>
 				</div>
 
 				{this.loggedin()}
