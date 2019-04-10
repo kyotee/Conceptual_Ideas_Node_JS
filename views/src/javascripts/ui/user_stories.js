a26d46264bfeeb1e7841d81d63f3edbb4944cc0d
tamkylet <kyle_tam1@hotmail.com>
2019-04-09 17:29:48 -0700

    ux+ui user stories work

--- views/src/javascripts/ui/user_stories.js
+++ views/src/javascripts/ui/user_stories.js
@@ -7,8 +7,20 @@ class UserStories extends Component {
 	}
 	render() {
 		return (
-			<div>
-				<p>User Stories Feature</p>
+			<div className="user-story">
+				<div className="title-container">
+					<div className="title-image"></div>
+					<p className="title">Conceptual Idea</p>
+				</div>
+				<div className="title-line"></div>
+				<div className="story-body">
+					<p>Given</p>
+					<p>A problem emerges.</p>
+					<p>When</p>
+					<p>Time is scarce.</p>
+					<p>Then</p>
+					<p>I must deal with it immediately.</p>
+				</div>
 			</div>
 		)
 	}
