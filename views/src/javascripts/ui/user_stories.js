fbddfeac73fd356676d4c5d2bc0c80e5a60f3028
tamkylet <kyle_tam1@hotmail.com>
2019-04-11 15:52:36 -0700

    edit story progress

--- views/src/javascripts/ui/user_stories.js
+++ views/src/javascripts/ui/user_stories.js
@@ -5,7 +5,22 @@ import "../../stylesheets/user_stories.scss";
 
 class UserStories extends Component {
 	componentDidMount() {
+		let title = document.getElementsByClassName('title')[0];
+		let editTitle = document.getElementsByClassName('edit-title')[0];
+		let bodyText = document.getElementsByClassName('body-text');
+		let EditBodyText = document.getElementsByClassName('edit-body-text');		
+		let edit = document.getElementsByClassName('editing')[0];
 
+		edit.addEventListener("click", function() {
+			title.classList.toggle('edit');
+			editTitle.classList.toggle('edit');
+			bodyText[0].classList.toggle('edit');
+			bodyText[1].classList.toggle('edit');
+			bodyText[2].classList.toggle('edit');
+			EditBodyText[0].classList.toggle('edit');
+			EditBodyText[1].classList.toggle('edit');
+			EditBodyText[2].classList.toggle('edit');
+		});
 	}
 	printDocument() {
 		const input = document.getElementsByClassName('user-story')[0];
@@ -26,20 +41,25 @@ class UserStories extends Component {
 						<div className="title-container">
 							<div className="title-image"></div>
 							<p className="title">Conceptual Idea</p>
+							<input className="edit-title" type="text"></input>
 						</div>
 						<div className="title-line"></div>
 						<div className="story-body">
-							<p>Given</p>
-							<p>A problem emerges.</p>
-							<p>When</p>
-							<p>Time is scarce.</p>
-							<p>Then</p>
-							<p>I must deal with it immediately.</p>
+							<p className="body-font">Given</p>
+							<p className="body-text">A problem emerges.</p>
+							<input className="edit-body-text"></input>
+							<p className="body-font">When</p>
+							<p className="body-text">Time is scarce.</p>
+							<input className="edit-body-text"></input>
+							<p className="body-font">Then</p>
+							<p className="body-text">I must deal with it immediately.</p>
+							<input className="edit-body-text"></input>
 						</div>
 					</div>
 				</div>
 
-				 <button onClick={this.printDocument}>Print</button>
+				<p className="printing" onClick={this.printDocument}>Print</p>
+				<p className="editing">Edit</p>
 			</div>
 		)
 	}
